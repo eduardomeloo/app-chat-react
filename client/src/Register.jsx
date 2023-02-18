a2dabca27410264eb7e10f70ef18035ade11696e
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-02-18 15:01:48 -0300

    Install axios and request frontend to backend

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
+import axios from 'axios'
 
 export default function Register() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
+    async function register(ev) {
+        ev.preventDefault()
+        await axios.post('/register', {username, password})
+    }
     return (
         <div className="bg-blue-50 h-screen flex items-center">
-            <form className="w-64 mx-auto mb-12">
+            <form className="w-64 mx-auto mb-12" onSubmit={register}>
                 <input  value={username} type="text" placeholder="username"
                         onChange={event => setUsername(event.target.value)} 
                         className="block w-full rounded-sm p-2 mb-2 border" />
