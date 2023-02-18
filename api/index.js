9a4194a9ce3f1ada5c6773881ad98fc6788f4466
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-02-18 16:30:27 -0300

    Ajuste

--- api/index.js
+++ api/index.js
@@ -6,10 +6,13 @@ const cors = require('cors')
 const User = require('./models/User');
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, (err) => {
+    if(err) throw err;
+});
 const jwtSecret = process.env.JWT_SECRET
 
 const app = express();
+app.use(express.json())
 app.use(cors({
     credentials: true,
     //origin: '*',
@@ -22,11 +25,19 @@ app.get('/test', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    const createdUser = await User.create({username, password});
-    jwt.sign({userId: createdUser._id}, jwtSecret, {}, (err, token) => {
-        if (err) throw err;
-        res.cookie('token', token).status(201).json('ok')
-    })
+    try {
+        const createdUser = await User.create({username, password});
+        jwt.sign({userId: createdUser._id}, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json({
+                id: createdUser._id
+            })
+        })
+    } catch(err) {
+        if(err) throw err;
+        res.status(500).json('error')
+    }
+    
 })
 
 app.listen(4000);
