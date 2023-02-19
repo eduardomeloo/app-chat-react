acb241dab37419608262b948d7ffd14059f556fb
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-02-19 15:34:16 -0300

    Configure Web Socket Server

--- api/index.js
+++ api/index.js
@@ -4,8 +4,9 @@ const cookieParser = require('cookie-parser')
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
-const User = require('./models/User');
 const bcrypt = require('bcryptjs')
+const User = require('./models/User');
+const ws = require('ws')
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, (err) => {
@@ -77,4 +78,27 @@ app.post('/register', async (req, res) => {
     
 })
 
-app.listen(4000);
+//app.listen(4000);
+
+const server = app.listen(4000);
+
+const wss = new ws.WebSocketServer({server})
+
+wss.on('connection', (connection, req) => {
+    const cookies = req.headers.cookie;
+    if(cookies) {
+        const tokenCookieString =  cookies.split(';').find(str => str.startsWith('token='));
+        if (tokenCookieString) {
+            const token = tokenCookieString.split('=')[1]
+            if (token) {
+                jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                    if (err) throw err;
+                    const {userId, username} = userData;
+                    connection.userId = userId;
+                    connection.username = username;
+                });
+            }
+        }
+    }
+    console.log([...wss.clients].map(c => c.username))
+})
