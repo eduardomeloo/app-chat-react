a69e873fcc3f959fe7ead58ea915e232deab1dac
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-02-21 21:55:52 -0300

    filter people online and ajust layout

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Avatar from "./Avatar";
 import Logo from "./Logo";
+import {UserContext} from './UserContext'
 export default function Chat() {
     const [ws, setWs] = useState(null);
     const [onlinePeople, setOnlinePeople] = useState({});
     const [selectedUserId, setSelectedUserId] = useState(null);
+    const {usernam, id} = useContext(UserContext)
 
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:4000');
@@ -32,15 +34,25 @@ export default function Chat() {
         console.log(selectedUserId)
     }
 
+    const onlinePeopleExclOurUser = {...onlinePeople};
+    delete onlinePeopleExclOurUser[id]
+
     return (
         <div className="flex h-screen">
             <div className="bg-white w-1/3">
                 <Logo />
-                {Object.keys(onlinePeople).map(userId => (
-                    <div onClick={() => setSelectedUserId(userId)} 
+                {Object.keys(onlinePeopleExclOurUser).map(userId => (
+                    <div key={userId} onClick={() => setSelectedUserId(userId)} 
                         className={"border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer "+(userId === selectedUserId ? 'bg-blue-50' : '')}>
-                        <Avatar username={onlinePeople[userId]} userId={userId} />
-                        <span className="text-gray-800">{onlinePeople[userId]}</span>
+                        {userId === selectedUserId && (
+                            <div className="w-1 bg-blue-500 h-12 rounded-r-md">
+
+                            </div>
+                        )} 
+                        <div className="flex gap-2 items-center py-2 pl-4">
+                            <Avatar username={onlinePeople[userId]} userId={userId} />
+                            <span className="text-gray-800">{onlinePeople[userId]}</span>
+                        </div>   
                     </div>
                 ))}
             </div>
