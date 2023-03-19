5574e12066266729fc0f0afc11506f6a0c55c3b7
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-03-19 19:33:01 -0300

    ajuste na responsividade

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -221,6 +221,32 @@ export default function Chat() {
             {!!selectedUserId && windowSize[0] <= 960 ? 
             (
                 <div className="flex flex-col bg-blue-50 p-2 w-full h-full">
+                    <div className="flex items-center cursor-pointer" onClick={() => setSelectedUserId(null)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path fillRule="evenodd" d="M20.25 12a.75.75 0 01-.75.75H6.31l5.47 5.47a.75.75 0 11-1.06 1.06l-6.75-6.75a.75.75 0 010-1.06l6.75-6.75a.75.75 0 111.06 1.06l-5.47 5.47H19.5a.75.75 0 01.75.75z" clipRule="evenodd" />
+                        </svg>
+
+                        {Object.keys(onlinePeopleExclOurUser).map(userId => (
+                            <Contact 
+                                key={userId}
+                                id={userId} 
+                                online={true}
+                                username={onlinePeopleExclOurUser[userId]}
+                                onClick={() => setSelectedUserId(null)}
+                                selected={userId === selectedUserId}
+                            />
+                        ))}
+                        {Object.keys(offlinePeople).map(userId => (
+                            <Contact 
+                                key={userId}
+                                id={userId} 
+                                online={false}
+                                username={offlinePeople[userId].username}
+                                onClick={() => setSelectedUserId(null)}
+                                selected={userId === selectedUserId}
+                            />
+                        ))}
+                    </div>
                     <div className="flex-grow">
                         {!!selectedUserId && (
                             <div className="relative h-full">
@@ -249,31 +275,6 @@ export default function Chat() {
                             </div>
                         )}
                     </div>
-                    {!!selectedUserId && (
-                        <form className="flex gap-2" onSubmit={sendMessage}>
-                        <input  type="text" 
-                                value={newMessageText}
-                                onChange={ev => setNewMessageText(ev.target.value)}
-                                placeholder="Type your message here" 
-                                className="bg-white flex-grow rounded-sm border p-2" />
-                        <label 
-                            className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
-                            <input  type="file" 
-                                    className="hidden"
-                                    onChange={async ev => await sendFile(ev)}
-                                    onClick={e => (e.target.value = null)}     
-                            />
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
-                            </svg>
-                        </label>
-                        <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                            </svg>
-                        </button>
-                    </form>
-                    )}
                 </div>
             )
             : null}
